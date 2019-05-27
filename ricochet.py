c4dcd2746041b8f30d72678f2738ca8952735f73
yannis <thimotheeleclerc@gmail.com>
2019-05-27 10:50:18 +0200

    correction

--- ricochet.py
+++ ricochet.py
@@ -8,7 +8,7 @@ SOUTH = 0X04
 WEST = 0x08
 ROBOT = 0X10
 
-REVERSE = [0 , SOUTH, WESTR,0,NORTH,0,0,0,EAST]
+REVERSE = [0 , SOUTH, WEST,0,NORTH,0,0,0,EAST]
 OFFSET = [0,-16,1,0,16,0,0,0,-1]
 
 # Game = (grid[256],moves[256],robots[4],token,last)
