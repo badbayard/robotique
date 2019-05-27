360b40eee450f183e6bfd7797393e3f912623f6a
yannis <thimotheeleclerc@gmail.com>
2019-05-27 09:47:33 +0200

    modif class ricochet.py

--- ricochet.py
+++ ricochet.py
@@ -11,9 +11,24 @@ ROBOT = 0X10
 REVERSE = [0 , SOUTH, WESTR,0,NORTH,0,0,0,EAST]
 OFFSET = [0,-16,1,0,16,0,0,0,-1]
 
-Game = (grid[256],moves[256],robots[4],token,last)
-Entry = (key,depth)
-Set = (mask,size)
+# Game = (grid[256],moves[256],robots[4],token,last)
+# Entry = (key,depth)
+# Set = (mask,size)
+
+class Game:
+    def __init__(self):
+        self.grid = [0] * 256
+        self.robots = [0] * 4
+        self.token = 0
+        self.last = 0
+class Entry:
+    def __init__(self):
+        self.key = 0
+        self.depth = 0
+class Set:
+    def __init__(self):
+        self.mask = 0
+        self.size = 0
 
 def swap(array, a , b):
     temp = array[a]
@@ -22,3 +37,4 @@ def swap(array, a , b):
 
 def make_key():
     global Game
+    robots = [0] * 4
