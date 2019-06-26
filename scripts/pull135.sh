61ebbaf075d36369d385a35a94d82cd31890e4b6
adrien <p1408826@forge.univ-lyon1.fr>
2019-06-26 17:08:50 +0200

    remove scripts

--- scripts/pull135.sh
+++
@@ -1,5 +0,0 @@
-scp -r -p robot@192.168.1.135:/home/robot/parcours/`ls [^__]*.py` ../parcoursRobot135/
-
-mv ../parcoursRobot135/parcours/* ../parcoursRobot135/
-
-rm -r ../parcoursRobot135/parcours
