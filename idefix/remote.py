780dfc689c19bb74aa5de995a412e59b8349758a
mathieuz38 <mathieu.zenner@etu.univ-lyon1.fr>
2019-06-27 18:12:20 +0200

    ajout self

--- idefix/remote.py
+++ idefix/remote.py
@@ -11,35 +11,33 @@ class RemoteBot(Bot):
         conf = robot_json_entry(hostname)
         super().__init__(name=hostname, color=conf['color'])
         serverPort = 2000                   # use arbitrary port > 1024
-        s = socket(AF_INET, SOCK_STREAM)    # create a TCP socket
-        s.connect((conf['ip'], serverPort)) # connect to server on the port
-
+        self.s = socket(AF_INET, SOCK_STREAM)    # create a TCP socket
+        self.s.connect((conf['ip'], serverPort)) # connect to server on the port
 
     def wall(self, dir: Union[Direction, RelativeDirection]) -> Wall:
         if isinstance(dir, Direction):
             dir = self.dir.get_relative(dir)
-        s.send(b'w '+dir.encode('utf-8')
-        
+        self.s.send(b'w '+dir.encode('utf-8'))
         raise NotImplementedError('wall')
 
     def stop(self, *args):
-        s.send(b'stop')
+        self.s.send(b'stop')
         
     def direction(self, *args, **kwargs):
-        d=""
-        s.send(b'd '+d.encode('utf-8'))
+        d = ""
+        self.s.send(b'd '+d.encode('utf-8'))
         raise NotImplementedError('direction')
                
     def forward(self, count: int = 1, *args, **kwargs) -> None:
-        s.send(b'f')
+        self.s.send(b'f')
         #raise NotImplementedError('forward')
 
     def turn_left(self, *args, **kwargs):
-        s.send(b'l')
+        self.s.send(b'l')
         #raise NotImplementedError('turn_left')
 
     def turn_right(self, *args, **kwargs):
-        s.send(b'r')
+        self.s.send(b'r')
         #raise NotImplementedError('turn_right')
 
     def write_info(self, board: Board, *args, **kwargs):
